ba84e650c73003639f1620b773efaa6537581a39
Leonardo <leowitcroz@gmail.com>
2023-01-26 17:08:57 -0300

    first commit

---
+++ php_action/delete.php
@@ -0,0 +1,27 @@
+<?php
+
+session_start();
+
+require_once 'db_connect.php';
+
+if(isset($_POST['btn-deletar'])):
+$nome = mysqli_escape_string($connect,$_POST['nome']);
+$sobrenome = mysqli_escape_string($connect,$_POST['sobrenome']);
+$email = mysqli_escape_string($connect,$_POST['email']);
+$idade = mysqli_escape_string($connect,$_POST['idade']);
+$id = mysqli_escape_string($connect, $_POST['id']);
+
+
+$sql = " DELETE FROM clientes WHERE id = '$id'";
+
+    if(mysqli_query($connect, $sql)):
+        $_SESSION['mensagem'] = "Deletado com sucesso";
+        header('Location: ../index.php');
+    else:
+        $_SESSION['mensagem'] = "Erro ao deletar";
+        header('Location: ../index.php');
+    endif;
+
+
+    
+endif;
